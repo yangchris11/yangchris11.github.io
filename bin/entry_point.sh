b07374321a3debdc18e1c0fb331f929ccb37dbc9
Chris Yang <chriyang@nvidia.com>
2026-09-15 11:05:57 -0700

    [security] narrow preview git trust scope

--- bin/entry_point.sh
+++ bin/entry_point.sh
@@ -4,7 +4,7 @@ CONFIG_FILE=_config.yml
 
 # Function to manage Gemfile.lock
 manage_gemfile_lock() {
-    git config --global --add safe.directory '*'
+    git config --global --add safe.directory /srv/jekyll
     if command -v git &> /dev/null && [ -f Gemfile.lock ]; then
         if git ls-files --error-unmatch Gemfile.lock &> /dev/null; then
             echo "Gemfile.lock is tracked by git, keeping it intact"
